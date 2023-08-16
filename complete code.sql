65cf77aa5cdf1ecca696aac92b6fb65d0ce6da51
Angel Zapata <126125206+azap2124@users.noreply.github.com>
2023-08-16 13:04:54 -0600

    Update complete code.sql

--- complete code.sql
+++ complete code.sql
@@ -14,7 +14,7 @@ ORDER BY units_sold DESC;
 -- Average rating counts
 -- Shows average rating count for all the products 
 SELECT AVG(rating_count)
-FROM wish. dbo.summer_products
+FROM wish. dbo.summer_products;
 
 -- Top 10 worst rated products
 -- Shows worst rated products with a rating count of 800 or more
@@ -43,7 +43,7 @@ FROM wish. dbo.summer_products
 WHERE title_orig IS NOT NULL
 ORDER BY units_sold DESC
 )
-AS avg_best_selling_products
+AS avg_best_selling_products;
 
 -- Average rating for least selling products
 SELECT AVG (rating) AS avg_least_selling_products
@@ -74,7 +74,7 @@ SELECT
 FROM wish. dbo.summer_products
 WHERE title_orig IS NOT NULL AND uses_ad_boosts = 1
 ) 
-AS avg_revenue_ads
+AS avg_revenue_ads;
 
 -- Average revenue for products that don't use ads
 SELECT ROUND(AVG(total_revenue),2) AS no_ads
@@ -86,7 +86,7 @@ SELECT
 FROM wish. dbo.summer_products
 WHERE title_orig IS NOT NULL AND uses_ad_boosts = 0
 ) 
-AS avg_revenue_no_ads
+AS avg_revenue_no_ads;
 
 
 /*What are the most common product tags and how do they relate to sales?*/
@@ -102,48 +102,42 @@ ORDER BY total_revenue DESC;
 
 /*Is there a relationship between merchant rating and product quality as indicated by the badges?*/
 -- Merchant rating and number of badges
-SELECT merchant_rating, badges_count
+SELECT COUNT(merchant_rating) AS merchants_with_badges
 FROM wish.dbo.summer_products
-WHERE badges_count >= 1
-ORDER BY merchant_rating DESC;
+WHERE badges_count >= 1;
 
 -- Merchant rating for badge non-holders
-SELECT merchant_rating, badges_count
+SELECT COUNT(merchant_rating) AS merchants_no_badges
 FROM wish.dbo.summer_products
-WHERE badges_count = 0
+WHERE badges_count = 0;
 
 -- Average rating for mechants with at least one badge
 SELECT AVG(merchant_rating) AS avg_rating
 FROM wish.dbo.summer_products
-WHERE badges_count >= 1
+WHERE badges_count >= 1;
 
 -- Average rating for badge non-holders
 SELECT AVG(merchant_rating) AS avg_rating
 FROM wish.dbo.summer_products
-WHERE badges_count = 0 
-
--- Mechants with the product quality badge
-SELECT merchant_rating, badge_product_quality
-FROM wish.dbo.summer_products
-WHERE badge_product_quality >= 1
-ORDER BY merchant_rating DESC;
+WHERE badges_count = 0;
 
 -- Average rating for merchants with the product quality badge
 SELECT AVG(merchant_rating) AS avg_rating
 FROM wish.dbo.summer_products
-WHERE badge_product_quality >= 1
+WHERE badge_product_quality = 1;
 
 -- Average rating for merchants that DO NOT hold product quality badge
 SELECT AVG(merchant_rating) AS avg_rating
 FROM wish.dbo.summer_products
-WHERE badge_product_quality = 0 
+WHERE badge_product_quality = 0;
+
 
 
 /*Do companies that sell to many countries have higher revenues?*/
 -- Average of countries_shipped_to
 -- Calculates the average of how many countries are shipped to per product
 SELECT AVG(countries_shipped_to) AS average_countries_shipped
-FROM wish.dbo.summer_products
+FROM wish.dbo.summer_products;
 
 -- Count of products that ship to less than 29 countries 
 SELECT COUNT(countries_shipped_to) AS low_countries
@@ -153,13 +147,13 @@ SELECT (units_sold * retail_price) AS total_revenue,
 countries_shipped_to,
 	CASE
 		WHEN (countries_shipped_to) < 29 THEN 'low'
-		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'medium'
+		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'average'
 		WHEN (countries_shipped_to) > 61 THEN 'high'
 	END countries_level
 FROM wish.dbo.summer_products
 )
 AS low_average
-WHERE countries_level = 'low'
+WHERE countries_level = 'low';
 
 -- Count of products that ship to more than 61 countries 
 SELECT COUNT(countries_shipped_to) AS high_countries
@@ -169,13 +163,13 @@ SELECT (units_sold * retail_price) AS total_revenue,
 countries_shipped_to,
 	CASE
 		WHEN (countries_shipped_to) < 29 THEN 'low'
-		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'medium'
+		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'average'
 		WHEN (countries_shipped_to) > 61 THEN 'high'
 	END countries_level
 FROM wish.dbo.summer_products
 )
 AS low_average
-WHERE countries_level = 'high'
+WHERE countries_level = 'high';
 
 -- Average revenue for products that ship to less than 29 countries 
 SELECT ROUND(AVG(total_revenue),2) AS avg_revenue_low 
@@ -185,13 +179,13 @@ SELECT (units_sold * retail_price) AS total_revenue,
 countries_shipped_to,
 	CASE
 		WHEN (countries_shipped_to) < 29 THEN 'low'
-		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'medium'
+		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'average'
 		WHEN (countries_shipped_to) > 61 THEN 'high'
 	END countries_level
 FROM wish.dbo.summer_products
 )
 AS low_average
-WHERE countries_level = 'low'
+WHERE countries_level = 'low';
 
 -- Average revenue for products that ship to more than 61 countries 
 SELECT ROUND(AVG(total_revenue),2) AS avg_revenue_high 
@@ -201,13 +195,13 @@ SELECT (units_sold * retail_price) AS total_revenue,
 countries_shipped_to,
 	CASE
 		WHEN (countries_shipped_to) < 29 THEN 'low'
-		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'medium'
+		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'average'
 		WHEN (countries_shipped_to) > 61 THEN 'high'
 	END countries_level
 FROM wish.dbo.summer_products
 )
 AS high_average
-WHERE countries_level = 'high'
+WHERE countries_level = 'high';
 
 -- Average revenue for products that ship to between 30 and 60 countries 
 SELECT ROUND(AVG(total_revenue),2) AS avg_revenue_medium
@@ -217,13 +211,14 @@ SELECT (units_sold * retail_price) AS total_revenue,
 countries_shipped_to,
 	CASE
 		WHEN (countries_shipped_to) < 29 THEN 'low'
-		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'medium'
+		WHEN (countries_shipped_to) BETWEEN 30 AND 60 THEN 'average'
 		WHEN (countries_shipped_to) > 61 THEN 'high'
 	END countries_level
 FROM wish.dbo.summer_products
 )
 AS medium_average
-WHERE countries_level = 'medium'
+WHERE countries_level = 'average';
+
 
 
 /*Is there a relationship between merchant profile pictures and sales?*/
